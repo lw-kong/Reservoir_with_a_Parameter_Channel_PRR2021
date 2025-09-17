3d9e71ad9271a6c023557f5658212a5b9c51cd88
Ling-Wei Kong <47471418+lw-kong@users.noreply.github.com>
2025-09-16 23:11:42 -0400

    Add files via upload

---
+++ System_Ikeda_Map/Ikeda_STP_10_predict.m
@@ -0,0 +1,70 @@
+addpath('\klw\Research\Functions');
+addpath('\klw\Research\Reservoir\PredictBehaviorUnderDiffPara\STP_reorganize_phase');
+
+
+warmup_r_step_cut = 20;
+warmup_r_step_length = 10;
+
+predict_r_step_cut = 10;
+predict_r_step_length = 2000;
+
+
+% 1.0027
+I_a = 1.01;
+tp = I_a;
+I_a_warmup = max(para_train_set);
+
+
+tmax_timeseries_warmup = (warmup_r_step_cut + warmup_r_step_length + 5 ) * reservoir_tstep;
+
+rng('shuffle');
+tic;
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% predict
+fprintf('predicting...\n');
+
+x0 = rand+1i*rand;
+x = zeros(tmax_timeseries_warmup,1);
+x(1) = x0;
+for t_i = 2:tmax_timeseries_warmup
+    x(t_i) = func_IHJM(x(t_i-1),I_a_warmup);
+end
+x = x(warmup_r_step_cut+1 : warmup_r_step_cut+warmup_r_step_length);
+ts_warmup = zeros(length(x),2);
+ts_warmup(:,1) = real(x);
+ts_warmup(:,2) = imag(x);
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+flag_r = [n dim a warmup_r_step_length predict_r_step_cut predict_r_step_length];
+predict_r = func_STP_predict(ts_warmup,tp_W * ( tp + tp_bias) ,W_in,res_net,P,flag_r);
+
+toc;
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% plot
+label_font_size = 12;
+ticks_font_size = 12;
+
+figure()
+subplot(2,1,1)
+plot( reservoir_tstep * (0:1:length(predict_r)-1) ,predict_r(:,1))
+xlabel('t','FontSize',label_font_size)
+ylabel('Re(z)','FontSize',label_font_size)
+title(['a =' num2str(I_a,8)])
+
+subplot(2,1,2)
+plot( reservoir_tstep * (0:1:length(predict_r)-1) ,predict_r(:,2))
+xlabel('t','FontSize',label_font_size)
+ylabel('Im(z)','FontSize',label_font_size)
+set(gca,'FontSize',ticks_font_size)
+set(gcf,'color','white')
+
+%
+figure()
+scatter(predict_r(:,1),predict_r(:,2),1)
+xlabel('Re(z)','FontSize',label_font_size)
+ylabel('Im(z)','FontSize',label_font_size)
+title(['prediction of reservoir' newline 'a =' num2str(I_a,8)])
+set(gcf,'color','white')
+grid on
+box on
+%
---
+++ System_Ikeda_Map/Ikeda_STP_10_train.m
@@ -0,0 +1,194 @@
+% chose one of the best bo training results
+% different tp_set and ode_parameter_set
+% warm up
+% no return map validation
+% control parameter is 1 dim
+
+addpath('\klw\Research\Functions');
+addpath('\klw\Research\Reservoir\PredictBehaviorUnderDiffPara\STP_reorganize_phase');
+
+
+dim = 2;
+
+% opt_Ikeda_SameData_20201104T135550_683.mat
+n = 800;
+k = 266;
+eig_rho = 0;
+W_in_a = 2.95;
+tp_W = 0.456;
+tp_bias = -3.00;
+a = 0.84;
+beta = 1.6 * 10^(-4);
+
+% opt_Ikeda_SameData_20201104T155128_337.mat
+n = 100;
+k = 91;
+eig_rho = 0;
+W_in_a = 2;
+tp_W = 0.21;
+tp_bias = 2.23;
+a = 1;
+beta = 1 * 10^(-8);
+
+% opt_Ikeda_SameData_20201104T151717_320.mat
+n = 400;
+k = 283;
+eig_rho = 0.17;
+W_in_a = 2.6;
+tp_W = 0.35;
+tp_bias = 0.47;
+a = 1;
+beta = 1 * 10^(-6);
+
+%{
+n = 400;
+k = 100;
+eig_rho = 0.2;
+W_in_a = 2;
+tp_W = 0.5;
+tp_bias = 0.5;
+a = 1;
+beta = 1 * 10^(-6);
+%}
+
+reservoir_tstep = 1;
+ratio_tstep = 1;
+
+train_r_step_cut = round( 20 / reservoir_tstep );
+train_r_step_length = round( 800 /reservoir_tstep );
+validate_r_step_length = round( 15 /reservoir_tstep );
+plot_dim = 2;
+%
+
+
+
+%validate_r_step_length = 15 /reservoir_tstep;
+bo = 5;
+
+
+
+para_train_set = [0.91 0.94 0.97];
+
+tp_train_set = para_train_set;
+
+
+
+tmax_timeseries_train = (train_r_step_cut + train_r_step_length + validate_r_step_length + 20) * reservoir_tstep; % time, for timeseries
+rng('shuffle');
+tic;
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% preparing training data
+rmse_min = 10000;
+for bo_i = 1:bo
+    fprintf('preparing training data...\n');
+    
+    train_data_length = train_r_step_length + validate_r_step_length + 10;
+    train_data = zeros(length(tp_train_set), train_data_length,dim+1); % data that goes into reservior_training
+    for tp_i = 1:length(tp_train_set)
+        tp = tp_train_set(tp_i);
+        I_a = para_train_set(tp_i);  %% system sensitive
+
+        x0 = rand+1i*rand;
+        x = zeros(tmax_timeseries_train,1);
+        x(1) = x0;
+        for t_i = 2:tmax_timeseries_train
+            x(t_i,:) = func_IHJM(x(t_i-1,:),I_a);
+        end
+        x = x(train_r_step_cut+1:end);
+        ts_train = zeros(length(x),2);
+        ts_train(:,1) = real(x);
+        ts_train(:,2) = imag(x);
+
+        
+        train_data(tp_i,:,1:dim) = ts_train(1:train_data_length,:);        
+        train_data(tp_i,:,dim+1) = tp_W * (tp + tp_bias) * ones(train_data_length,1);    %% system sensitive        
+    end
+    
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % train
+    fprintf('training...\n');
+    flag_r_train = [n k eig_rho W_in_a a beta train_r_step_cut train_r_step_length validate_r_step_length...
+        reservoir_tstep dim plot_dim];
+    [rmse,W_in_temp,res_net_temp,P_temp,t_validate_temp,x_real_temp,x_validate_temp] = ...
+        func_STP_train(train_data,tp_train_set,flag_r_train,1,1,1);
+    fprintf('attempt rmse = %f\n',rmse)
+    
+    if rmse < rmse_min
+        W_in = W_in_temp;
+        res_net = res_net_temp;
+        P = P_temp;
+        t_validate = t_validate_temp;
+        x_real = x_real_temp;
+        x_validate = x_validate_temp;        
+        rmse_min = rmse;
+    end
+    
+    fprintf('%f is done\n',bo_i/bo)
+    toc;
+end
+
+fprintf('best rmse = %f\n',rmse_min)
+
+plot_dim = 1; % change the ylabel
+for tp_i = 1:length(tp_train_set)
+    figure('Name','Reservoir Predict');
+    subplot(2,1,1)
+    hold on
+    plot(t_validate,x_real(tp_i,:,plot_dim));
+    plot(t_validate,x_validate(tp_i,:,plot_dim),'--');
+    xlabel('time');
+    ylabel('V');
+    title(['tp = ' num2str( para_train_set(tp_i),6 )]);
+    set(gcf,'color','white')
+    hold off
+    subplot(2,1,2)
+    hold on
+    plot(t_validate,abs(x_validate(tp_i,:,plot_dim)-x_real(tp_i,:,plot_dim))/...
+        ( max(x_real(tp_i,:,plot_dim)) - min(x_real(tp_i,:,plot_dim)) ) )
+    line([t_validate(1) t_validate(end)],[0.05 0.05])
+    xlabel('time');
+    ylabel('relative error');
+    hold off
+end
+
+%{
+plot_dim = 1; % change the ylabel
+for tp_i = 1:length(tp_train_set)
+    figure('Name','Reservoir Predict');
+    subplot(2,1,1)
+    hold on
+    plot(x_real(tp_i,:,plot_dim));
+    plot(x_validate(tp_i,:,plot_dim),'--');
+    xlabel('time');
+    ylabel('V');
+    title(['tp = ' num2str( para_train_set(tp_i),6 )]);
+    set(gcf,'color','white')
+    hold off
+    subplot(2,1,2)
+    hold on
+    plot(abs(x_validate(tp_i,:,plot_dim)-x_real(tp_i,:,plot_dim))/...
+        ( max(x_real(tp_i,:,plot_dim)) - min(x_real(tp_i,:,plot_dim)) ) )
+    line([t_validate(1) t_validate(end)],[0.05 0.05])
+    xlabel('step');
+    ylabel('relative error');
+    hold off
+end
+%}
+
+%
+% plotting training data
+figure('Name','Training Data','Position',[50 50 480 390]);
+hold on
+for tp_i = 1:length(tp_train_set)
+    
+    
+    scatter(train_data(tp_i,:,1),train_data(tp_i,:,2),1.5);
+    xlabel('\delta_m');
+    ylabel('V');
+    title(['training data at' newline 'tp = ' num2str( para_train_set(tp_i),8 )]);
+    set(gcf,'color','white')
+    
+end
+hold off
+
+%
